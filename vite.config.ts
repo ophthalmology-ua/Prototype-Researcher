7d3539dedeeaf74817e59cb71dea5fd9414b3af1
lautigr116 <lreinoso@mail.austral.edu.ar>
2025-08-11 16:15:24 -0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Prototype-Researcher/",
   server: {
     host: "::",
     port: 8080,
