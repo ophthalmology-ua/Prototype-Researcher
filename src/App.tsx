907eca0154e744ad95520ef799e782c30653f95d
lautigr116 <lreinoso@mail.austral.edu.ar>
2025-08-11 18:05:06 -0300

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
               <Route path="/aggregates" element={<Aggregates />} />
               <Route path="/patients" element={<Patients />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
+              <Route path="*" element={<Dashboard />} />
             </Routes>
           </AppLayout>
         </HelmetProvider>
